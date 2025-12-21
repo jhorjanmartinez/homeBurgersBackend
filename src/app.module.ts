d6720a9b070bdfe3778eecd465528e6b69498602
jhorjanmartinez <jhorjanmartinez13@hotmail.com>
2025-12-20 23:31:23 -0500

    plantilla con conexion a docker,nest js con postgres y el category resource creado

---
+++ src/app.module.ts
@@ -0,0 +1,30 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CategoriesModule } from './categories/categories.module';
+
+@Module({
+  imports: [
+    // Carga las variables de entorno desde .env
+    ConfigModule.forRoot(),
+
+    // Configuración de la base de datos usando variables de entorno
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT!,
+      database: process.env.DB_NAME,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      autoLoadEntities: true,
+      synchronize: true, //solo para desarrollo
+    }),
+
+    CategoriesModule,
+  ],
+  controllers: [],
+  providers: [
+    
+  ],
+})
+export class AppModule {}
